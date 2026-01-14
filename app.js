582d6ec157edb67e7347c9ddbd7c2d3968724e3e
rrend224 <ryuku422rem@icloud.com>
2026-01-14 12:06:53 +0900

    Implement custom cursor hover effects
    Added hover effects for cursor on anchor and button elements.

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-anchor.addEventListener('click', e => {
-e.preventDefault();
-document.querySelector(anchor.getAttribute('href')).scrollIntoView({
-behavior: 'smooth'
-});
-});
-});
-
 const cursor = document.querySelector('.cursor');
+const hoverTargets = document.querySelectorAll('a, button');
 
 document.addEventListener('mousemove', (e) => {
   cursor.style.top = e.clientY + 'px';
   cursor.style.left = e.clientX + 'px';
+});
+
+hoverTargets.forEach(el => {
+  el.addEventListener('mouseenter', () => {
+    cursor.classList.add('is-hover');
+  });
+  el.addEventListener('mouseleave', () => {
+    cursor.classList.remove('is-hover');
+  });
 });
